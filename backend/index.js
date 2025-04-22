3f89df39ed445baa37eb5f2dabcd23bd96bf681f
rupertjamessimpson <87886963+rupertjamessimpson@users.noreply.github.com>
2025-04-22 00:30:54 -0400

    Pushing front and back end

--- backend/index.js
+++ backend/index.js
@@ -8,15 +8,54 @@ import cors from "cors";
 import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 import bcrypt from 'bcryptjs';
+import fetch from 'node-fetch';
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://cryptidcrossing.org"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
+
 const prisma = new PrismaClient();
+
+// Test route
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+// Imagur API call
+app.get("/api/imgur", async (req, res) => {
+  try {
+    const response = await fetch("https://api.imgur.com/3/album/SwMk7LH/images", {
+      headers: {
+        Authorization: "Client-ID 84c97bbf2c0f9d1",
+      },
+    });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Imgur fetch failed", error);
+    res.status(500).json({ error: "Failed to fetch images from Imgur" });
+  }
+});
 
 // Middleware to verify JWT token sent by the client
 function requireAuth(req, res, next) {
@@ -32,11 +71,6 @@ function requireAuth(req, res, next) {
     return res.status(401).json({ error: "Unauthorized: Invalid token" });
   }
 }
-
-// Test route
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
 
 // Login route
 app.post("/login", async (req, res) => {
@@ -139,7 +173,37 @@ app.post("/shows", async (req, res) => {
   }
 });
 
+// PUT route for editing a show
+app.put("/shows/:id", async (req, res) => {
+  const { image_url, description, venue, month, day, year, published } = req.body;
+
+  try {
+    const updatedShow = await prisma.show.update({
+      where: { id: parseInt(req.params.id) },
+      data: { image_url, description, venue, month, day, year, published },
+    });
+
+    res.json(updatedShow);
+  } catch (error) {
+    console.error("Update error:", error);
+    res.status(500).json({ error: "Failed to update show" });
+  }
+});
+
+// DELETE route
+app.delete("/shows/:id", async (req, res) => {
+  try {
+    await prisma.show.delete({
+      where: { id: parseInt(req.params.id) },
+    });
+
+    res.json({ message: "Show deleted" });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to delete show" });
+  }
+});
+
 // Listening notification
 app.listen(8000, () => {
-  console.log("Server running on http://localhost:8000 🎉 🚀");
+  console.log("Server running! 🎉 🚀");
 });
